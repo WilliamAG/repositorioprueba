66ea5ba2a780e338410eb184adbf75124990734d
WilliamAG <88340168+WilliamAG@users.noreply.github.com>
2021-08-10 23:47:06 -0600

    Borrado

--- Sistema_Registro_Universitario/src/main/java/proyecto/unah/bd/repository/RepositorySeccionLab.java
+++
@@ -1,9 +0,0 @@
-package proyecto.unah.bd.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import proyecto.unah.bd.model.SeccionLab;
-
-public interface RepositorySeccionLab extends JpaRepository <SeccionLab, Integer>{
-	public SeccionLab findById(int id);
-}
